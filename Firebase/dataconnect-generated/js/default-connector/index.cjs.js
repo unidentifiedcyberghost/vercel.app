c855be6c03a05da266514a5996090676da6e5577
unidentifiedcyberghost <n0ns3ns31988@gmail.com>
2026-06-08 12:05:38 +0800

    Delete Firebase directory

--- Firebase/dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,9 +0,0 @@
-const { getDataConnect, validateArgs } = require('firebase/data-connect');
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'Firebase',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
-
